ac23473419effb390bf0f2a90eb5345fa367ec7c
reinholz36 <dreinholz28@gmail.com>
2022-03-23 13:47:12 -0400

    offline-functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin;
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 // const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
 // const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require('path');
@@ -14,7 +13,7 @@ const config = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: __dirname + '/dist'
+    path: `${__dirname}/dist`
   },
   module: {
     rules: [
